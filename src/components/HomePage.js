e191109db2e5776240969e91e02ecf9d90ab1c56
AlexTertyshnyk <alex112244@gmail.com>
2019-12-26 21:21:00 +0200

    navigation with postback to server

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,6 +5,7 @@ function HomePage() {
     <div className="jumbotron">
       <h1>Pluralsight Administration</h1>
       <p>React, Flux and Reacr Router for responsive web apps.</p>
+      <a href="/about">About</a>
     </div>
   );
 }

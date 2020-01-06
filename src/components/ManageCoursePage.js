20447b46174362815639b2d6724e9024261bd830
AlexTertyshnyk <alex112244@gmail.com>
2020-01-06 21:34:29 +0200

    added prompt example

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 
 const ManageCoursePage = props => {
   return (
     <>
       <h2>Manage Course</h2>
+      <Prompt when={true} message="are you sure?" />
       {props.match.params.slug}
     </>
   );

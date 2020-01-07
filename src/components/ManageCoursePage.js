686237ab71be9ad443cca28f1076402636d599e7
AlexTertyshnyk <alex112244@gmail.com>
2020-01-07 14:09:04 +0200

    added error handling and edit course

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import { Prompt } from "react-router-dom";
 import { toast } from "react-toastify";
 import * as courseApi from "../api/courseApi";
 import CourseForm from "./CourseForm";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: "",
@@ -12,6 +13,15 @@ const ManageCoursePage = props => {
     authorId: null,
     category: ""
   });
+
+  useEffect(() => {
+    (async () => {
+      const slug = props.match.params.slug;
+      if (slug) {
+        setCourse(await courseApi.getCourseBySlug(slug));
+      }
+    })();
+  }, [props.match.params.slug]);
 
   function handleChange({ target }) {
     const updatedCourse = {
@@ -21,8 +31,30 @@ const ManageCoursePage = props => {
     setCourse(updatedCourse);
   }
 
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) {
+      _errors.title = "Title is required";
+    }
+    if (!course.authorId) {
+      _errors.authorId = "Author is required";
+    }
+    if (!course.category) {
+      _errors.category = "Category is required";
+    }
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
   async function handleSubmit(event) {
     event.preventDefault();
+    if (!formIsValid()) {
+      return;
+    }
+
     await courseApi.saveCourse(course);
     props.history.push("/courses");
     toast.success("Course saved");
@@ -33,6 +65,7 @@ const ManageCoursePage = props => {
       <h2>Manage Course</h2>
       {/* <Prompt when={true} message="are you sure?" /> */}
       <CourseForm
+        errors={errors}
         course={course}
         onChange={handleChange}
         onSubmit={handleSubmit}
